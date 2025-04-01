8932d9cc0b79534cc18dbf1886810e449647e7b4
Lucas Menezes <lucasmenezesba@gmail.com>
2025-04-01 19:39:59 -0300

    Adiciona exemplo

--- src/components/SbButton.vue
+++ src/components/SbButton.vue
@@ -15,7 +15,24 @@ const handleClick = function () {
 
 <style scoped>
 button {
-  background-color: red;
-  color: black;
+  padding: 12px;
+  border-radius: 8px;
+  border: none;
+  outline: none;
+  box-shadow: 0 3px 6px #00000015;;
+  background-image: linear-gradient(109.9deg,  rgba(0,174,182,1) 4.5%, rgba(190,223,224,1) 22.6%, rgba(217,198,225,1) 45.1%, rgba(248,178,54,1) 68.5%, rgba(235,105,17,1) 87.2% );
+  color: #FFF;
+  font-weight: bold;
+  font-size: 18px;
+  font-family: 'Fantasque Sans Mono';
+  &:hover {
+    filter: brightness(1.05);
+    cursor: pointer;
+  }
+  &:active {
+    filter: brightness(0.95);
+    box-shadow: none;
+    translate: 1px 1px;
+  }
 }
 </style>
